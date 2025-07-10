b2c3b7e37925266183a386f774e630db4253e857
afonso.araujo <afonso.araujo@supertroco.com.br>
2025-07-09 22:40:54 -0300

    feat: safe cors

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://chat-comp-front.vercel.app'
+}));
 app.use(express.json());
 
 // Conectar ao MongoDB
